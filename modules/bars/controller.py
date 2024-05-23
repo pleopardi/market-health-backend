369c3d5ae2ad3d46e5af7d0d1cd83a42654124b9
Paolo Leopardi <leopardipaolo@gmail.com>
2024-05-23 21:56:55 +0200

    refactor(bars): extract repository

--- modules/bars/controller.py
+++ modules/bars/controller.py
@@ -1,13 +1,8 @@
-from alpaca.data.requests import StockBarsRequest
-from alpaca.data.timeframe import TimeFrame, TimeFrameUnit
-from alpaca.data.models import BarSet
-from datetime import datetime, timedelta
 from fastapi import APIRouter, Depends
 from pydantic import BaseModel, Field
 from typing import Literal
 from .Bar import Bar
-from .mappers import bar_set_to_bar_list
-from .stock_client import make_stock_client
+from .repository import BarsRepository
 
 router = APIRouter(prefix="/bars")
 
@@ -18,28 +13,8 @@ class _QueryParams(BaseModel):
 
 @router.get("/{symbol}")
 def get_bars(
-    symbol: str, params: _QueryParams = Depends(), client=Depends(make_stock_client)
+    symbol: str,
+    params: _QueryParams = Depends(),
+    repository=Depends(BarsRepository),
 ) -> list[Bar] | None:
-    result = client.get_stock_bars(
-        StockBarsRequest(
-            end=datetime.now() - timedelta(days=1),
-            start=datetime.now()
-            - (
-                timedelta(days=365)
-                if params.model_dump().get("timeframe") == "day"
-                else timedelta(weeks=260)
-            ),
-            symbol_or_symbols=symbol,
-            timeframe=TimeFrame(
-                1,
-                TimeFrameUnit.Day
-                if params.model_dump().get("timeframe") == "day"
-                else TimeFrameUnit.Week,
-            ),
-        )
-    )
-
-    if isinstance(result, BarSet):
-        return bar_set_to_bar_list(result, symbol)
-
-    return None
+    return repository.get_bars(symbol, params.model_dump().get("timeframe"))
---
+++ modules/bars/repository.py
@@ -0,0 +1,35 @@
+from alpaca.data.requests import StockBarsRequest
+from alpaca.data.timeframe import TimeFrame, TimeFrameUnit
+from alpaca.data.models import BarSet
+from datetime import datetime, timedelta
+from fastapi import Depends
+from typing import Literal
+from .Bar import Bar
+from .mappers import bar_set_to_bar_list
+from .stock_client import make_stock_client
+
+
+class BarsRepository:
+    def __init__(self, client=Depends(make_stock_client)):
+        self._client = client
+
+    def get_bars(
+        self, symbol: str, timeframe: Literal["day", "week"]
+    ) -> list[Bar] | None:
+        result = self._client.get_stock_bars(
+            StockBarsRequest(
+                end=datetime.now() - timedelta(days=1),
+                start=datetime.now()
+                - (timedelta(days=365) if timeframe == "day" else timedelta(weeks=260)),
+                symbol_or_symbols=symbol,
+                timeframe=TimeFrame(
+                    1,
+                    TimeFrameUnit.Day if timeframe == "day" else TimeFrameUnit.Week,
+                ),
+            )
+        )
+
+        if isinstance(result, BarSet):
+            return bar_set_to_bar_list(result, symbol)
+
+        return None
