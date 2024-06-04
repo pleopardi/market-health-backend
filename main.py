c8183f081ac8e838aa05a0a5b5d2f6998d6280c9
Paolo Leopardi <leopardipaolo@gmail.com>
2024-06-04 11:44:05 +0200

    feat: add CORS policy

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 from dotenv import load_dotenv
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import modules.bars as bars
 
 load_dotenv()
 
 app = FastAPI()
 app.include_router(bars.router)
+
+origins: list[str] = [
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 if __name__ == "__main__":
